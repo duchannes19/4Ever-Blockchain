87d0cdfb75b7c043961cc996908b4d7c05d5ee1c
Andrea Massignan <andreamassignan98@gmail.com>
2023-12-06 12:15:11 +0100

    FrontEnd

---
+++ 4everfrontend/src/services/blockchainService.js
@@ -0,0 +1,14 @@
+import Web3 from 'web3';
+
+const getWeb3 = async () => {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable();
+    return window.web3;
+  } else {
+    alert('Please install MetaMask');
+    return null;
+  }
+};
+
+export default getWeb3;
