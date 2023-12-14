8ef7f727e2aae30f8640363a6d69c23be3f8fe37
Andrea Massignan <andreamassignan98@gmail.com>
2023-12-14 16:32:14 +0100

    Changed Port

--- 4everbackend/index.js
+++ 4everbackend/index.js
@@ -1,7 +1,7 @@
 //Requirements
 require('dotenv').config();
 var cors = require('cors')
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const ganacheRpcEndpoint = process.env.GANACHE;
 const FourEverAddress = process.env.MARKETADDR;
 const FourEverABI = require('./Truffle/build/contracts/FourEver.json').abi;
