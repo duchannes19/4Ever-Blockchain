b3c6815a9e6201f92bc806a0e3e097bafe4cecce
CesareDev <corsicesare.lavoro@gmail.com>
2024-01-11 13:14:35 +0100

    Add script for creating/cleaning the companies db

--- 4everbackend/database/Database.js
+++ 4everbackend/database/Database.js
@@ -1,29 +1,95 @@
-const fs = require("fs");
+require('dotenv').config({ path: __dirname + '/../.env' });
 const path = require("path");
+const { Web3 } = require('web3');
+var Datastore = require('nedb');
 
-const questsDBPath = path.resolve(__dirname, "./quests.db");
+var questsDatabase = new Datastore({ filename: path.join(__dirname, './quests.db'), autoload: true });
+var companiesDatabase = new Datastore({ filename: path.join(__dirname, './companies.db'), autoload: true });
 
-const questsStringDatabase =
-    '{"name":"Hunt for the Lost Relic","description":"Embark on a journey to find the ancient relic hidden deep in the Forbidden Forest.","startDate":"2024-11-01","expirationDate":"2024-12-31","participants":[],"usersThreshold":5,"questRegistered":false,"questEnded":false,"winner":null,"_id":"2tUoTnXMnOuGeyFY"}\n' +
-    '{"name":"Save the Princess","description":"The princess has been kidnapped by the evil wizard. Rescue her from the wizard\'s castle.","startDate":"2024-10-01","expirationDate":"2024-10-31","participants":[],"usersThreshold":5,"questRegistered":false,"questEnded":false,"winner":null,"_id":"IqmirDmo7SKz5Y3z"}\n' +
-    '{"name":"Defeat the Dragon","description":"A fearsome dragon has been terrorizing the kingdom. Slay the dragon and bring peace to the land.","startDate":"2024-10-15","expirationDate":"2024-11-15","participants":[],"usersThreshold":5,"questRegistered":false,"questEnded":false,"winner":null,"_id":"qRysykZABKuTuBCa"}\n';
+//CesareDev: Create / Clean the quest database after usage
+questsDatabase.remove({}, { multi: true }, (err) => {
+    if (err) {
+        console.log({
+            success: false,
+            message: 'Error',
+            body: err
+        });
+    }
+    else {
+        questsDatabase.persistence.compactDatafile();
+        const quests =
+            [
+                { "name": "Hunt for the Lost Relic", "description": "Embark on a journey to find the ancient relic hidden deep in the Forbidden Forest.", "startDate": "2024-11-01", "expirationDate": "2024-12-31", "participants": [], "usersThreshold": 5, "questRegistered": false, "questEnded": false, "winner": null },
+                { "name": "Save the Princess", "description": "The princess has been kidnapped by the evil wizard. Rescue her from the wizard\'s castle.", "startDate": "2024-10-01", "expirationDate": "2024-10-31", "participants": [], "usersThreshold": 5, "questRegistered": false, "questEnded": false, "winner": null },
+                { "name": "Defeat the Dragon", "description": "A fearsome dragon has been terrorizing the kingdom. Slay the dragon and bring peace to the land.", "startDate": "2024-10-15", "expirationDate": "2024-11-15", "participants": [], "usersThreshold": 5, "questRegistered": false, "questEnded": false, "winner": null }
+            ];
+        questsDatabase.insert(quests, (err, docs) => {
+            if (err) {
+                console.log({
+                    success: false,
+                    message: 'Error',
+                    body: err
+                });
+            }
+            else if (docs) {
+                console.log('====================================');
+                console.log('Quests Database:');
+                console.log();
+                console.log({
+                    success: true,
+                    message: 'Quests database ready!',
+                    body: docs
+                });
+            }
+        });
+    }
+});
 
-console.log("=============================");
-console.log();
-if (fs.existsSync(questsDBPath)) {
-    console.log("quests.db file found! -> clearing");
+//CesareDev: Create / Clean the companies database after usage
+function assign(acc) {
+    if (acc.length > 2) {
+        companiesDatabase.remove({}, { multi: true }, (err) => {
+            if (err) {
+                console.log({
+                    success: false,
+                    message: 'Error',
+                    body: err
+                });
+            }
+            else {
+                companiesDatabase.persistence.compactDatafile();
+                const companies =
+                    [
+                        { "name": "Blizzard Entertainment", "address": acc[acc.length - 1] },
+                        { "name": "Activision", "address": acc[acc.length - 2] },
+                        { "name": "Riot Games", "address": acc[acc.length - 3] }
+                    ];
+                companiesDatabase.insert(companies, function (err, docs) {
+                    if (err) {
+                        console.log({
+                            success: false,
+                            message: 'Error',
+                            body: err
+                        });
+                    }
+                    else if (docs) {
+                        console.log('====================================');
+                        console.log('Companies Database:');
+                        console.log();
+                        console.log({
+                            success: true,
+                            message: 'Companies database ready!',
+                            body: docs
+                        });
+                    }
+                });
+            }
+        });
+    }
 }
-else {
-    console.log("quests.db file not found! -> creation");
-}
-try {
-    fs.writeFileSync(questsDBPath,
-        questsStringDatabase
-    );
-    console.log("Done!");
-    console.log();
-}
-catch (err) {
-    console.log('Error:');
-    console.log(err);
-}
+
+const web3 = new Web3(process.env.GANACHE);
+
+//CesareDev: Using the above function we can retrieve all the account of the chain
+//           and pass them in the assign func
+web3.eth.getAccounts().then(assign).catch((err) => { console.log(err) });
